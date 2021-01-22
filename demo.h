b361bcb14b3401e79eee51bd205836079ae0e865
d4vvv <45372078+d4vvv@users.noreply.github.com>
2021-01-22 20:35:01 +0100

    Delete demo.h

--- demo.h
+++
@@ -1,9 +0,0 @@
-#include "MKL05Z4.h"
-
-void Demo_Color(uint8_t Time, uint8_t Br, uint8_t Color, uint8_t tbl[8][8]);
-void Demo1_Color(uint8_t Time, uint8_t Br, uint8_t Color, uint8_t tbl[8][8]);
-void Demo2_Color(uint8_t Time, uint8_t Br, uint8_t Color, uint8_t tbl[8][8]);
-void Demo_Contour(uint8_t Color, uint8_t Time, uint8_t Br, uint8_t tbl[8][8], uint8_t rotations);
-void Rectangle(uint8_t Time, uint8_t Br, uint8_t Color, uint8_t tbl[8][8], uint8_t x1, uint8_t y1, uint8_t z1, uint8_t x2, uint8_t y2, uint8_t z2);
-void Demo_XXXX(uint8_t Time, uint8_t Color, uint8_t Br);
-void Demo3_Color(uint8_t Time);
